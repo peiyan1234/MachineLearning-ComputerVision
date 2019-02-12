dbcf8e80dda46194f5efcc0de6f94c8381195285
peiyan1234 <d05548014@ntu.edu.tw>
2019-02-12 16:10:25 +0800

    Add files via upload

--- Labels_correction_tool.py
+++ Labels_correction_tool.py
@@ -1,209 +1,198 @@
 import json
 import os
-import fnmatch
+import glob
 import numpy as np
 from PIL import Image
-import matplotlib.cm as cm
 
-##### Preface #####
+"""
+This tool is created by Alvin Pei Yan, Li, whose email addresses are Alvin.Li@acer.com and d05548014@ntu.edu.tw.
+Please contact to him, the author, if any problems or requirments has been found or asked.
+2019.02.12 for the 2nd version.
+"""
 
-# This tool is created by Alvin Pei Yan, Li, whose email addresses are Alvin.Li@acer.com and d05548014@ntu.edu.tw.
-# Please contact to him, the author, if any problems or requirments has been found or asked.
-# 2019.01.29 for the 1st version.
+"Global Variables"
 
-##### Function Definitions #####
+global dir_labels
+global dir_savingfiles
+
+"Function Definitions"
 
 def main():
-    #
-    # this is the main function handling all sub-functions together
-    #
+    """
+    This is the main function handling all sub-functions together.
+    """
     print('Start.\n')
+    goback_dir = os.getcwd()
 
-    global_IDtable = get_global_table_as_reference()
-    namelist_local_table = get_a_list_of_local_tables_for_correction()
-    do_correction_for_local_lablefiles(namelist_local_table, global_IDtable)
+    global_IDtable = get_global_table()
+    correct_local_labelfiles(global_IDtable)
 
+    os.chdir(goback_dir)
     print('\nDone.')
 
-def get_global_table_as_reference():
-    #
-    # get the correction table for correcting labels and return it as a global_IDtable
-    # the name: label_to_id
-    # the suffix: .json
-    # the format: hash table
-    #
-    with open('label_to_id.json', encoding='utf-8-sig') as json_file:
-        print('label_to_id.json has been read.')
+def get_global_table():
+    """
+    get the correction table for correcting labels and return it as a global_IDtable
+    the name: label_to_id
+    the suffix: .json
+    the format: hash table
+    """
+    try:
+        with open('label_to_id.json', encoding='utf-8-sig') as json_file:
+            print('label_to_id.json has been read.')
 
-        global_IDtable = json.load(json_file)
+            global_IDtable = json.load(json_file)
+    except IOError:
+            print('An error occured trying to read {}.'.format('label_to_id.json'))
 
     return global_IDtable
 
-def get_a_list_of_local_tables_for_correction():
-    #
-    # get a list of the local tables stored the wrong keys that should be corrected and return it as namelist_local_table
-    # whereis? : 'labels/'
-    # the name: DaVincixxxxxxxx_y_z; x: Date, e.g. 20181213; y:a number with several digits; z: 0 typically
-    # the suffix: .json
-    # the format: hash table
-    #
-    os.chdir('labels/')
+def correct_local_labelfiles(global_IDtable):
+    """
+    do the label correction for all the label files in labels/ and store the corrected files in the subdir correct/
+    """
+    def correct_single_npyfile(local_label_arr, local_label_filename, local_label_table, global_IDtable):
+        """
+        do the correction mapping for each local .npy label file
+        """
 
-    namelist_local_table = []
-    for file in os.listdir('.'):
-        if fnmatch.fnmatch(file,'*.json'):
-            print('{} has been found.'.format(file))
-
-            namelist_local_table.append(str(file))
-
-    os.chdir('..')
-    return namelist_local_table
-
-def do_correction_for_local_lablefiles(namelist_local_table, global_IDtable):
-    #
-    # do the label correction for all the label files in labels/ and store the corrected files in the subdir correct/
-    #
-    def submain():
-        #
-        # realize the functionality of 'do_correction_for_local_lablefiles'
-        #
-        os.chdir('labels/')
-        os.mkdir('correct')
-
-        for filename in namelist_local_table:
-            local_label_filename = filename.replace(".json","")
-            local_label_arr = np.load(local_label_filename + '.npy')
-            with open(filename, encoding='utf-8-sig') as json_file:
-                local_label_table = json.load(json_file)
-
-            corrected_label_arr = do_for_single_npyfile(local_label_arr, local_label_filename, local_label_table, global_IDtable)
-            name_of_new_seg_im = generate_corrected_seg(corrected_label_arr, local_label_filename, global_IDtable)
-            generate_overlayimage(name_of_new_seg_im, local_label_filename)
-
-        os.chdir('..')
-
-    def do_for_single_npyfile(local_label_arr, local_label_filename, local_label_table, global_IDtable):
-        #
-        # do the correction mapping for each local .npy label file
-        #
-        
         corrected_label_arr = np.zeros(local_label_arr.shape)
-        
+
         for key in list(local_label_table.keys()):
             corrected_label_arr[local_label_arr == int(key)] = global_IDtable[local_label_table[key]]
 
-        new_label_filename = local_label_filename.replace(".npy", "") + '_n'
+        new_label_filename = local_label_filename + '_n'
 
-        np.save('correct/' + new_label_filename, corrected_label_arr)
+        savingdir = dir_savingfiles + '/' + new_label_filename
+        np.save(savingdir, corrected_label_arr)
 
         print('{} has been saved.'.format(new_label_filename))
 
         return corrected_label_arr
 
     def generate_corrected_seg(corrected_label_arr, local_label_filename, global_IDtable):
-        #
-        # generate a corrected segamenation mask
-        #        
-        
-        #normalized_label_arr = cm.nipy_spectral(corrected_label_arr)
-        #normalized_label_arr *= 255.0/normalized_label_arr.max()
-        
-        #new_seg_im = Image.fromarray(np.uint8(normalized_label_arr))
-        
-        color_dic = { 'Black' : [0, 0, 0],
-                      'White' : [255, 255, 255],
-                      'Medium Gray' : [128, 128, 128],
-                      'Aqua' : [0, 128, 128],
-                      'Navy Blue' : [0, 0, 128],
-                      'Green' : [0, 255, 0],
-                      'Orange' : [255, 165, 0],
-                      'Yellow' : [255, 255, 0],
-                      'Maroon' : [128, 0, 0]
-                    }
-        
-        #new_seg_im = Image.fromarray(corrected_label_arr).convert('RGB')
-        
-        frameshape = list(corrected_label_arr.shape)
-        colored_new_seg_im = np.zeros((frameshape[0], frameshape[1], 3))
-        
-        stacked_for_boolean_masks = np.stack((corrected_label_arr,corrected_label_arr,corrected_label_arr), axis = 2)
-        
-        inform_Background = int(global_IDtable['Background'])
-        inform_Peritoneum = int(global_IDtable['Peritoneum'])
-        inform_Ovary = int(global_IDtable['Ovary'])
-        inform_Uterus = int(global_IDtable['Uterus'])
-        inform_Fallopian_Tube = int(global_IDtable['Fallopian_Tube'])
-        inform_Ligament = int(global_IDtable['Ligament'])
-        inform_Ureter = int(global_IDtable['Ureter'])
-        inform_Artery = int(global_IDtable['Artery'])
-        inform_Scapel = int(global_IDtable['Scapel'])
-        
-        Background = (stacked_for_boolean_masks == [inform_Background, inform_Background, inform_Background]).all(axis = 2)
-        Peritoneum = (stacked_for_boolean_masks == [inform_Peritoneum, inform_Peritoneum, inform_Peritoneum]).all(axis = 2)
-        Ovary = (stacked_for_boolean_masks == [inform_Ovary, inform_Ovary, inform_Ovary]).all(axis = 2)
-        Uterus = (stacked_for_boolean_masks == [inform_Uterus, inform_Uterus, inform_Uterus]).all(axis = 2)
-        Fallopian_Tube = (stacked_for_boolean_masks == [inform_Fallopian_Tube, inform_Fallopian_Tube, inform_Fallopian_Tube]).all(axis = 2)
-        Ligament = (stacked_for_boolean_masks == [inform_Ligament, inform_Ligament, inform_Ligament]).all(axis = 2)
-        Ureter = (stacked_for_boolean_masks == [inform_Ureter, inform_Ureter, inform_Ureter]).all(axis = 2)
-        Artery = (stacked_for_boolean_masks == [inform_Artery, inform_Artery, inform_Artery]).all(axis = 2)
-        Scapel = (stacked_for_boolean_masks == [inform_Scapel, inform_Scapel, inform_Scapel]).all(axis = 2)
-        
-        colored_new_seg_im[Background] = color_dic['Black'] 
-        colored_new_seg_im[Peritoneum] = color_dic['Maroon']
-        colored_new_seg_im[Ovary] = color_dic['Medium Gray']
-        colored_new_seg_im[Uterus] = color_dic['White']
-        colored_new_seg_im[Fallopian_Tube] = color_dic['Navy Blue']
-        colored_new_seg_im[Ligament] = color_dic['Green']
-        colored_new_seg_im[Ureter] = color_dic['Orange']
-        colored_new_seg_im[Artery] = color_dic['Yellow']
-        colored_new_seg_im[Scapel] = color_dic['Aqua']
-        
-        name_of_new_seg_im = local_label_filename + '_n_seg.jpg'
-        
-        new_seg_im = Image.fromarray(np.uint8(colored_new_seg_im)).convert('RGB')
-        
-        new_seg_im.save('correct/' + name_of_new_seg_im)
+        """
+        generate a corrected segamenation mask
+        """
 
-        #new_seg_im.convert('RGB').save('correct/' + name_of_new_seg_im)
+        frameshape = list(corrected_label_arr.shape)
+        colored_new_seg_im = np.zeros((frameshape[0], frameshape[1], 3), dtype = np.uint8) # BRK: dtype:uint8
+
+        stacked_for_boolean_masks = np.stack((corrected_label_arr,corrected_label_arr,corrected_label_arr), axis = 2)
+
+        ID_dict = get_ID_dict(global_IDtable)
+        for tissue_name in list(ID_dict.values()):
+            tissueID = int(global_IDtable[tissue_name])
+            tissue_mask = get_tissue_mask(tissueID, stacked_for_boolean_masks)
+            colored_new_seg_im[tissue_mask] = draw_color(tissue_name)
+
+        name_of_new_seg_im = local_label_filename + '_n_seg.jpg'
+
+        new_seg_im = Image.fromarray(np.uint8(colored_new_seg_im))
+
+        savingdir = dir_savingfiles + '/' + name_of_new_seg_im
+        new_seg_im.save(savingdir)
 
         print('{} has been saved.'.format(name_of_new_seg_im))
 
         return name_of_new_seg_im
 
+    def get_ID_dict(global_IDtable):
+
+        ID_dict = dict(zip(range(len(list(global_IDtable.keys()))),list(global_IDtable.keys())))
+
+        return ID_dict
+
+    def get_tissue_mask(tissueID, stacked_for_boolean_masks):
+
+        tissue_mask = (stacked_for_boolean_masks == [tissueID, tissueID, tissueID]).all(axis = 2)
+
+        return tissue_mask
+
+    def draw_color(tissue_name):
+
+        RGB_dict = { 'Black' : [0, 0, 0],
+                     'White' : [255, 255, 255],
+                     'Medium Gray' : [128, 128, 128],
+                     'Aqua' : [0, 128, 128],
+                     'Navy Blue' : [0, 0, 128],
+                     'Green' : [0, 255, 0],
+                     'Orange' : [255, 165, 0],
+                     'Yellow' : [255, 255, 0],
+                     'Maroon' : [128, 0, 0]
+                    }
+
+        tissue_color = { 'Background' : 'Black',
+                         'Peritoneum' : 'Maroon',
+                         'Ovary' : 'Medium Gray',
+                         'Uterus' : 'White',
+                         'Fallopian_Tube' : 'Navy Blue',
+                         'Ligament' : 'Green',
+                         'Ureter' : 'Orange',
+                         'Artery' : 'Yellow',
+                         'Scapel' : 'Aqua',
+                       }
+
+        RGB_color = RGB_dict[tissue_color[tissue_name]]
+
+        return RGB_color
+
     def generate_overlayimage(name_of_new_seg_im, local_label_filename):
-        #
-        # generate an image overlaying the original image with the corresponding mask
-        #
+        """
+        generate an image overlaying the original image with the corresponding mask
+        """
         name_of_original_image = local_label_filename + '.jpg'
 
-        original_im = Image.open(name_of_original_image)
+        open_dir_original = dir_labels + '\\' + name_of_original_image
+        original_im = Image.open(open_dir_original)
 
-        os.chdir('correct/')
-
-        new_seg_im = Image.open(name_of_new_seg_im)
-        original_im = original_im.convert('RGB')
-        new_seg_im = new_seg_im.convert('RGB')
+        open_dir_new = dir_savingfiles + '\\' + name_of_new_seg_im
+        new_seg_im = Image.open(open_dir_new)
+        original_im = original_im
+        new_seg_im = new_seg_im
 
         overlay_image = Image.blend(original_im, new_seg_im, 0.4)   # 0.4 is the transparent rate.
 
         name_of_overlay_image = local_label_filename + '_n_overlay.jpg'
-        overlay_image.convert('RGB').save(name_of_overlay_image)
+        savingdir = dir_savingfiles + '/' + name_of_overlay_image
+        overlay_image.save(savingdir)
 
         print('{} has been saved.'.format(name_of_overlay_image))
 
-        os.chdir('..')
+    """
+    realize the functionality of 'correct_local_labelfiles'
+    """
+    os.chdir(dir_labels)
+    os.mkdir(dir_savingfiles)
 
-    submain()
+    for filename in glob.glob('*.json'):
+        print(filename + 'has been found.')
+        local_label_filename = filename.replace(".json","")
+        local_label_arr = np.load(local_label_filename + '.npy')
 
-##### Execution #####
+        try:
+            with open(filename, encoding='utf-8-sig') as json_file:
+                local_label_table = json.load(json_file)
+        except IOError:
+            print('An error occured trying to read {}.'.format(filename))
+
+        corrected_label_arr = correct_single_npyfile(local_label_arr, local_label_filename, local_label_table, global_IDtable)
+        name_of_new_seg_im = generate_corrected_seg(corrected_label_arr, local_label_filename, global_IDtable)
+        generate_overlayimage(name_of_new_seg_im, local_label_filename)
+
+""" Execution """
+
+dir_labels = input('Input the path of labels directory: ')
+dir_savingfiles = input('Input the path of the directory created for saveing files: ')
 
 import time
-start = time. time()
+if __name__ == '__main__':
+    start = time. time()
 
-main()
+    main()
 
-end = time. time()
+    end = time. time()
 
-duration = end - start
+    duration = end - start
 
-print('\nThis code runs so fast that only spends {} in second.'.format(duration))
+    print('\nThis code runs so fast that only spends {} in second.'.format(duration))
